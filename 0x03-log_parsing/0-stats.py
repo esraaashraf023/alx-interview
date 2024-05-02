01f51cb93a44f3af6401caa40df3d64a00ca59fb
esraaashraf023 <esraaashraf780567@gmail.com>
2024-05-02 21:46:25 +0300

    UPdata

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,42 +1,41 @@
 #!/usr/bin/python3
 """script reads stdin line"""
 import sys
-import re
 
+def print_stats(file_size, stats):
+    """ print stats """
+    print(f'File size: {file_size}')
+    for code, count in sorted(stats.items()):
+        if count != 0:
+            print(f'{code}: {count}')
 
 def main():
-    """ function.. """
-    fileSize = 0
+    """ main function """
+    file_size = 0
     count = 0
     codes = ['200', '301', '400', '401', '403', '404', '405', '500']
-    stats = {i: 0 for i in codes}
-
-    def print_stats(fileSize, stats):
-        """ print stats """
-        print(f'File size: {fileSize}')
-        for k in sorted(stats.items()):
-            if status[key] != 0:
-                print(f'{k}: {status[k]}')
+    stats = {code: 0 for code in codes}
 
     try:
         for line in sys.stdin:
             count += 1
             data = line.split()
             try:
-                status_code += int(data[-1])
-                if status_code in stats:
-                    stats[status_code] += 1
-            except BaseException:
+                status_code = int(data[-2])
+                if str(status_code) in stats:
+                    stats[str(status_code)] += 1
+            except ValueError:
                 pass
             try:
-                fileSize += int(data[-1])
-            except BaseException:
+                file_size += int(data[-1])
+            except ValueError:
                 pass
             if count % 10 == 0:
-                print_stats(stats, fileSize)
-        print_stats(stats, fileSize)
+                print_stats(file_size, stats)
+        print_stats(file_size, stats)
     except KeyboardInterrupt:
-        print_stats(stats, fileSize)
+        print_stats(file_size, stats)
         raise
-    if __name__ == "__main__":
-        main()
+
+if __name__ == "__main__":
+    main()
